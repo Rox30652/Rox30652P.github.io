d95329178de426e58990a8613a7e7eb94a361265
Rox30652 <124225394+Rox30652@users.noreply.github.com>
2023-03-06 16:47:52 -0300

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('boton').onclick = function () {
+    console.log("Lista de cursos");
+    document.getElementById("demo").innerHTML = "Otros cursos: *Curso de Diseño de Videojuegos: Producción y programación integral. *Análisis de datos y Gestión de Dashboards con Tableau. *Fundamentos de las Bases de Datos.";
+}
